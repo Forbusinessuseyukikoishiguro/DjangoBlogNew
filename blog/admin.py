5d3080891d5d0340fb134cbd9610a4c40d52395d
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-22 17:12:04 +0900

    03221711管理画面から日記作成

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted')
+    list_filter = ('date_posted', 'author')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'date_posted'
+    ordering = ('-date_posted',)
+
+admin.site.register(Post, PostAdmin)
--- myblog/urls.py
+++ myblog/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', include('blog.urls')),  # ホームページをルートに設定
+    # ここを変更: as_viewの引数にredirect_authenticated_userとnext_pageを追加
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html',
+        next_page='login'
+    ), name='logout'),
+    path('', include('blog.urls')),
 ]
