83d4289befa3f0baa164f48dfd26ea95577bd2b6
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-22 20:38:29 +0900

    032211801_ログアウト処理

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
-from django.contrib.auth import views as auth_views
+
+# LogoutViewを拡張してGETリクエストを許可するクラスを作成
+class CustomLogoutView(auth_views.LogoutView):
+    http_method_names = ['get', 'post']
 
 urlpatterns = [
-    # 既存のURLパターン
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # カスタムログアウトビューを使用
+    path('logout/', CustomLogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('myblog/', include('blog.urls')),
 ]
 
