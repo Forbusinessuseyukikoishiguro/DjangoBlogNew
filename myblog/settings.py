999284f8dfdd055b9c320fcd47282989755e208d
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-25 15:28:49 +0900

    03251528_Vercel設定

--- myblog/settings.py
+++ myblog/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+#03/25静的ファイルの設定を確認
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -26,8 +30,11 @@ SECRET_KEY = "django-insecure-+%v#^%$3$fnb21+@c+hsw-t4#en*%2mv_ekx7rrlgp$0o(t*@j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
-
+#ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = [
+    '*',
+    '.vercel.app'
+]
 
 # Application definition
 
@@ -92,12 +99,17 @@ WSGI_APPLICATION = "myblog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# 本番環境ではデータベースを使用しない場合
+if 'VERCEL' in os.environ:
+    DATABASES = {}
+# それ以外の場合は通常のデータベース設定を使用
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
